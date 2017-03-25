6b1e83e64488cb90eaf8c92cc1261e814c9f02de
DreamCatcher13 <gerey37ng@gmail.com>
2017-03-25 21:28:51 +0200

    Починаю робити практичну

---
+++ src/main/java/ua/edu/uzhnu/practice3/EncodingResult.java
@@ -0,0 +1,27 @@
+package ua.edu.uzhnu.practice3;
+
+import java.util.Map;
+
+/**
+ * Created by Таня on 25.03.2017.
+ */
+public class EncodingResult {
+
+    String encodedMessage;
+    Map<String, Character> charTable;
+
+    public EncodingResult(String encodedMessage, Map<String, Character> charTable) {
+        this.encodedMessage = encodedMessage;
+        this.charTable = charTable;
+    }
+
+    public String getEncodedMessage() {
+        return encodedMessage;
+    }
+/*
+    public Map<String, Character> getCharTable() {
+        Map<String, Character> copy = new Map<>(charTable);
+        copy = charTable;
+        return copy;
+    }*/
+}
---
+++ src/main/java/ua/edu/uzhnu/practice3/HuffmanEncoder.java
@@ -0,0 +1,17 @@
+package ua.edu.uzhnu.practice3;
+
+import java.util.Map;
+
+/**
+ * Created by Таня on 25.03.2017.
+ */
+public class HuffmanEncoder {
+
+   public EncodingResult encode(String Message){
+
+   }
+
+   public String decode (String Message, Map<String, Character> charTable ){
+       
+   }
+}
